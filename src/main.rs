17f800f286e8244f55a22183876dc6a2229b450e
yijiahe <yi.jiahe00@gmail.com>
2022-07-08 04:02:03 +0000

    Refactor error handling in main to handle errors rather than passing them up

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ use std::env;
 
 use leetcode_cli::endpoints;
 
-fn main() -> Result<(), std::env::VarError> {    
+fn main() {    
     dotenv().ok();
 
-    let csrftoken = env::var("CSRFTOKEN")?;
+    let csrftoken = match env::var("CSRFTOKEN") {
+        Ok(value) => value,
+        Err(error) => match error {
+            env::VarError::NotPresent => panic!("CSRFTOKEN variable not set"),
+            env::VarError::NotUnicode(_value) => panic!("CSRFTOKEN value: invalid"),
+        },
+    };
 
-    endpoints::get_daily(&csrftoken);
-
-    Ok(())
+    let daily_question = match endpoints::get_daily(&csrftoken) {
+        Ok(result) => result, 
+        Err(error) => panic!("{}", error),
+    };
 }
