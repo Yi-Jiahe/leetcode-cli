fff6391d8906331ee6fb0300794cda45936527dd
yijiahe <yi.jiahe00@gmail.com>
2022-07-08 04:03:35 +0000

    Fix erroneous x-csrftoken string

--- src/lib.rs
+++ src/lib.rs
@@ -28,14 +28,12 @@ pub mod endpoints {
     }";
 
     pub fn get_daily(csrftoken: &str) -> Result<(), reqwest::Error> {
-        println!("{}", csrftoken);
-
         let cookie = format!("csrftoken={};", csrftoken);
 
         let mut headers = reqwest::header::HeaderMap::new();
         headers.insert("referer", "https://leetcode.com/problemset/all/".parse().unwrap());
         headers.insert("cookie", cookie.parse().unwrap());
-        headers.insert("x-csrftoken", format!("{};", csrftoken).parse().unwrap());
+        headers.insert("x-csrftoken", format!("{}", csrftoken).parse().unwrap());
 
 
         let client = reqwest::blocking::Client::builder()
